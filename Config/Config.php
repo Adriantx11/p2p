0217dd0522669c240ded32cd3e0d018aa0ded24f
Adriantx11 <villagomezadrian498@gmail.com>
2025-08-04 23:35:14 -0600

    Add configuration files: Include Config.php and Vars.php in repository
    - Added Config.php with bot configuration
    - Added Vars.php with variable extraction logic
    - Updated .gitignore to allow config files
    - Configuration files now included for easier deployment

---
+++ Config/Config.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Bot Token
+|--------------------------------------------------------------------------
+|
+| Change this to your Bot API Token
+| It can be obtained from https://telegram.dog/BotFather
+|
+*/
+$config['botToken'] = "7768854053:AAH-thvVjeMaIyVQ1HXq64uoNouf1P9RuVM";
+
+/*
+|--------------------------------------------------------------------------
+| Admin User ID
+|--------------------------------------------------------------------------
+|
+| Change this to Admin's Numeric User ID
+| ID can be obtained from https://telegram.dog/username_to_id_bot
+|
+*/
+$config['adminID'] = "5073251387";
+
+/*
+|--------------------------------------------------------------------------
+| Logs Chat ID
+|--------------------------------------------------------------------------
+|
+| Create a New Channel/Group for logging data
+| ID can be obtained from https://telegram.dog/BotFather
+|
+*/
+$config['ChatID'] = "-1002451923844";
+
+/*
+|--------------------------------------------------------------------------
+| Logs Channel ID
+|--------------------------------------------------------------------------
+|
+| Create a New Channel/Group for logging data
+| ID can be obtained from https://telegram.dog/BotFather
+|
+*/
+$config['logsID'] =  "-1002451923844";
+
+/*
+|--------------------------------------------------------------------------
+| Timezone
+|--------------------------------------------------------------------------
+|
+| Current timezone for Logging Activities with time
+| It can be obtained from http://1min.in/content/international/time-zones
+| By Default it's in IST
+|
+*/
+$config['timeZone'] =  "Asia/Kolkata";
+
+/*
+|--------------------------------------------------------------------------
+| Database
+|--------------------------------------------------------------------------
+| SQLite Database to Store User Data
+|
+*/
+$config['db']['type'] = "sqlite";
+$config['db']['path'] = "database/siesta.db";
+
+/*
+|--------------------------------------------------------------------------
+| Anti-Spam Timer
+|--------------------------------------------------------------------------
+|
+| Anti-Spam Timer to prevent Spammers from Spamming the Checker
+| Value is in Seconds. "20" = 20seconds
+|
+*/
+$config['anti_spam_timer'] =  "30";
+
+?> 
